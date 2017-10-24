3f9fcec93cd0c5b0a32a83865f39d1cb5c0e2f9d
Erika <eyardumian@gmail.com>
2017-10-23 17:29:00 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GifTastic
+
+This application uses the GIPHY API to make a dynamic web page that populates with gifs of your choice. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,111 @@
-$("button").on("click", function() {
-var dog = $(this).attr('data');
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + dog + "&apikey=e9d5c73e287e4685b260b860ad5462e6";
-
-$.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .done(function(response) {
-        var results = response.data;
 
 
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var dogImage = $("<img class = 'dogPictures'>");
+var dogs = ['Dachshunds', 'Labradors', 'Poodles', 'Shetland Sheepdogs', 'Pugs', 'Golden Retrievers',
+'Dalmations', 'Beagles', 'Siberian Huskies', 'Chow Chow', 'Pomeranian', 'Great Dane'];
+
+
+function renderButtons() {
+  $('#dogButtons').empty();
+
+        for (var i = 0; i < dogs.length; i++) {
+          $('#dogButtons').append('<button>' + dogs[i] + '</button>');
+
+        /*  var button = $('<button>');
+          button.addClass('dog');
+          button.attr('data-name'), dogs[i];
+          button.text(dogs[i]);
+          $('#dogButtons').append(button);
+
+          var dogDiv = $('<div class = 'dog'>');
+          var rating = response.Rated;
+          var ratingDisplay = $('<p>').text('Rating:' + rating);
+          dogDiv.append(ratingDisplay);
+          var image = $('<img>').attr('src', imgURL)
+          dogDiv.append(image);
+          $('#dogPics').prepend(dogDiv);
+
             dogImage.attr("src", results[i].images.fixed_height.url);
             gifDiv.prepend(p);
             gifDiv.prepend(dogImage);
-
-            $("#dogPics").prepend(gifDiv);
+            $("#dogButtons").prepend(gifDiv);*/
         };
-      });
+      };
+      $('#add-dog').on('click', function(event){
+        event.preventDefault();
+        var dogInput = $('#animal-input').val().trim();
+        dogs.push(dogInput);
+        $('#animal-input').val(' ');
+        renderButtons();
 
-var dogList = 12;
-$("#add-dog").on("click", function(event) {
-    event.preventDefault();
-//grab the value of the input box and place it in a new button
+      })
+
+function getGifs() {
+      var dog = $(this).attr('data-name');
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dog + "&rating=pg&limit=10&apikey=e9d5c73e287e4685b260b860ad5462e6";
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+
+    .done(function(response) {
 
 
-    });
+
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+        var dogDiv = $('<div>');
+
+        var image = $('<img>').attr('src', imgURL);
+
+          image.attr('src', results[i].images.fixed_height_still.url)
+          image.attr('data-still', results[i].images.fixed_height_still.url)
+          image.attr('data-animate', results[i].images.fixed_height.url)
+          image.attr('data-state', 'still');
+          dogDiv.append(image);
+
+        $('#dogPics').prepend(dogDiv);
+
+        //dogDiv.append(image);
+        //$('#dogPics').prepend(dogDiv);
+
+    }
+    })
+};
+
+
+
+/*function displayGifs() {
+
+        var dog = $('#animal-input').val();
+        $("#add-dog").on("click", function(event) {
+          event.preventDefault();
+
+
+          dog++;
+          dogs.push(dog);
+
+
+
+    })
+  };*/
 
 //$('dogPics').on('click', function() {
-$(".dogPictures").on('click', function() {
-  var state=$(this).attr('data-state');
-  console.log(state);
-  alert(state);
-      var src;
-      var newState;
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
+    $(document).on('click', '.gifs', function() {
+      var state=$(this).attr('data-state');
+      console.log(state);
+        //var src;
+        //var newState;
+        if (state === "still") {
+          var animatedSource = $(this).attr('data-animate')
+          $(this).attr("src", animatedSource);
+          $(this).attr("data-state", "animate");
+        } else {
+          var stillSource = $(this).attr('data-still')
+          $(this).attr("src", stillSource);
+          $(this).attr("data-state", "still");
+        }
     });
-});
+    $(document).ready(function(){
+      renderButtons();
+    })
